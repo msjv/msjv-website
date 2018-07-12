b635046e7f00dcae6924b7067f2605f7445f9701
Markus Tran <thebluepillow@gmail.com>
2018-07-11 22:00:30 -1000

    init website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import SE6 from './components/SE6.jsx'
+import './App.scss'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <SE6 />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/SE6.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import styles from './SE6.scss'
+
+class App extends Component {
+  render () {
+    return (
+      <div className={styles.se6}>
+        :3c
+      </div>
+    )
+  }
+}
+
+export default App
