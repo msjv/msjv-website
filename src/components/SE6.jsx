35b61c3729e98d632be2b08d9961e434fd802c78
Markus Tran <thebluepillow@gmail.com>
2018-07-17 20:33:36 -1000

    init dada

--- src/components/SE6.jsx
+++ src/components/SE6.jsx
@@ -1,25 +1,54 @@
 import React, { Component } from 'react'
+import cn from 'classnames'
 
 import se6 from './SE6.ogg'
+import dadaluma from './dadaluma.png'
 import styles from './SE6.scss'
 
 class App extends Component {
   constructor (props) {
     super(props)
 
+    this.state = {
+      runProgram: false
+    }
+
+    this.counter = -1
     this.se6 = new window.Audio(se6)
     this.onClickSE6 = this.onClickSE6.bind(this)
   }
 
   onClickSE6 () {
-    this.se6.play()
-      .catch(error => console.error(error))
+    if (!this.state.runProgram) {
+      this.setState({
+        runProgram: true,
+        flipped: ++this.counter & 1
+      })
+
+      this.se6.play()
+        .catch(error => console.error(error))
+
+      this.endProgram = setTimeout(() => {
+        this.setState({ runProgram: false })
+        this.endProgram = null
+      }, 2500)
+    }
+  }
+
+  componentWillUnmount () {
+    if (this.endProgram) {
+      clearTimeout(this.endProgram)
+    }
   }
 
   render () {
     return (
       <div className={styles.se6} onClick={this.onClickSE6}>
         :3c
+        <img className={cn(styles.dadaluma, {
+          [styles.runProgram]: this.state.runProgram && !this.state.flipped,
+          [styles.runProgramFlipped]: this.state.runProgram && this.state.flipped
+        })} src={dadaluma} />
       </div>
     )
   }
