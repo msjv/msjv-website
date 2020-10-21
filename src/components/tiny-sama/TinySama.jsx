7724143c95e950ec6e7da2757d0351fd795a343f
eashaw <caglc@live.com>
2020-10-20 21:34:19 -0500

    tiny change

--- src/components/tiny-sama/TinySama.jsx
+++ src/components/tiny-sama/TinySama.jsx
@@ -5,7 +5,7 @@ import teeny from './18.png'
 class TinySama extends Component {
   render () {
     return( 
-    <div style={{textAlign:'center', backgroundColor: 'black', color: "pink", borderRadius: '30px'}}>
+    <div style={{textAlign:'center', backgroundColor: 'black', color: "pink", borderRadius: '30px', minHeight: '90vh'}}>
       <h2>tiny sama</h2>
       < img src={teeny} />
       <h3>dragoon o:</h3>
