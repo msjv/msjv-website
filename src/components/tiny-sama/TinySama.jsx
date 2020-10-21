00218950865b1206574e145367d3eb329e46edaa
eashaw <caglc@live.com>
2020-10-20 20:50:33 -0500

    tiny page

--- src/components/tiny-sama/TinySama.jsx
+++ src/components/tiny-sama/TinySama.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 
+import teeny from './18.png'
+
 class TinySama extends Component {
   render () {
-    return <div>Tiny Sama</div>
+    return( 
+    <div style={{textAlign:'center', backgroundColor: 'black', color: "pink", borderRadius: '30px'}}>
+      <h2>tiny sama</h2>
+      < img src={teeny} />
+      <h3>dragoon o:</h3>
+    </div>
+    )
   }
 }
 
